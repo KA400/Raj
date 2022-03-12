4a78f170808cebbfb5161fae848033cb7a674f87
KA400 <101449667+KA400@users.noreply.github.com>
2022-03-12 10:06:57 +0200

    Add files via upload

---
+++ Cipher.cs
@@ -0,0 +1,15 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine("What is your message?");
+        string realMsg = Console.ReadLine();
+        encrMsg = Program.Encrypt(string);
+	}
+    static string Encrypt(string msg)
+    {
+        int key = Random.RandInt
+    }
+}
